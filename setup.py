ef577bf912be72ed51877bbe3262ca3413272993
Alexander Solovyov <alexander@solovyov.net>
2011-02-10 10:27:20 +0100

    fix pip install convoread

--- setup.py
+++ setup.py
@@ -17,7 +17,8 @@ setup(
     author_email = 'foobarbuzz@googlegroups.com',
 
     url = 'https://github.com/foobarbuzz/convoread',
-    download_url = 'git+https://github.com/foobarbuzz/convoread.git',
+    download_url = ('git+https://github.com/foobarbuzz/convoread.git'
+                    '#egg=convoread-%s' % convoread.__version__) ,
 
     classifiers = [
         'Environment :: Console',
